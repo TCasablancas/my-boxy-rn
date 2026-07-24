937e37efdbdc398b8e92cc40a6e102048b68d6b6
Thyago Casablancas <thyago_casablancas@hotmail.com>
2026-07-24 14:23:34 -0300

    files updated for cache & build

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import 'react-native-get-random-values';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PlatformPressable } from '@react-navigation/elements';
 import { MainTabParamList, RootStackParamList } from './app/navigation/types';
 import { navigationRef, setMainNavigationReady } from './app/common/navigation/MainNavigation';
-import MBMainBottomsheet from './app/components/bottomsheet/MBMainBottomsheet';
 import { useAppHooks } from './app/AppHooks';
 import { NeutralColors, PrimaryColors } from './app/common/colors/Colors';
+import SplashScreen from 'react-native-splash-screen';
 
+import MBMainBottomsheet from './app/components/bottomsheet/MBMainBottomsheet';
 import UserHomeView from './app/views/userhome/UserHomeView';
 import UserFavoritesView from './app/views/favorites/UserFavoritesView';
 import MoreConfigsView from './app/views/moreconfigs/MoreConfigsView';
@@ -21,9 +24,7 @@ import MoreConfigsPlaceholderView from './app/views/moreconfigs/MoreConfigsPlace
 import ProductDetailView from './app/views/productdetail/ProductDetailView';
 import UserSignupView from './app/views/usersignup/UserSignupView';
 import { UserHomeStoreSignupView } from './app/views/userhome/UserHomeNavigation';
-import { StyleSheet, View } from 'react-native';
 import MBFloatingCartBtn from './app/components/buttons/MBFloatingCartBtn';
-import { useState } from 'react';
 
 const Tab = createBottomTabNavigator<MainTabParamList>();
 const RootStack = createNativeStackNavigator<RootStackParamList>();
@@ -51,7 +52,11 @@ const MORE_CONFIG_PATHS = [
 ] as const;
 
 function MainTabs() {
-  const mainColor = PrimaryColors.primary;
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  
   const {
     allowProtectedTabAccess,
     hasRegisteredStore,
@@ -79,7 +84,7 @@ function MainTabs() {
 
   const tabScreenOptions = {
     tabBarInactiveTintColor: 'gray',
-    tabBarActiveTintColor: mainColor,
+    tabBarActiveTintColor: PrimaryColors.primary,
     tabBarLabelStyle: {
       fontSize: 11,
       fontFamily: 'SNPro-Bold',
